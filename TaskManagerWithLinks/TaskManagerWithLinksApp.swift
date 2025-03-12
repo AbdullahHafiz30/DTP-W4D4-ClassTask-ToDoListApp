f2f6bd3675ddff8ef5652f75a8419b4b49a81ade
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-12 13:07:04 +0300

    Initial Commit

---
+++ TaskManagerWithLinks/TaskManagerWithLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerWithLinksApp.swift
+//  TaskManagerWithLinks
+//
+//  Created by Abdullah Hafiz on 12/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerWithLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
